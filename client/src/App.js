fae1876aac64a5ba6d987da44ca0ba48e1f827a4
siemowit <IDawid923@gmail.com>
2022-05-26 17:48:35 +0200

    commit 1

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,17 @@ function App() {
 
   const [imie, setImie] = useState('')
   const [nazwisko, setNazwisko] = useState('')
+  const [pesel, setPesel] = useState('')
+  const [miejsce_zamieszkania, setMiejsce_zamieszkania] = useState('')
+  const [numer_telefonu, setNumer_telefonu] = useState('')
 
   const submitKlient = () => {
     Axios.post("http://localhost:8002/api/insert", {
       imie: imie, 
       nazwisko: nazwisko,
+      pesel: pesel, 
+      miejsce_zamieszkania: miejsce_zamieszkania,
+      numer_telefonu: numer_telefonu, 
   }).then(() => {
     alert("Udało się");
     });
@@ -22,6 +28,7 @@ function App() {
       <h2>Rejestracja</h2>
 
       <div className='form'>
+
       <label>Imie:</label>
       <input type="text" name="imie" onChange={(e)=> {
 setImie(e.target.value)
@@ -30,8 +37,21 @@ setImie(e.target.value)
       <input type="text" name="nazwisko" onChange={(e)=> {
 setNazwisko(e.target.value)
 }}/>
+      <label>Pesel:</label>
+      <input type="text" name="pesel" onChange={(e)=> {
+setPesel(e.target.value)
+}}/>
+      <label>Miejsce zamieszkania:</label>
+      <input type="text" name="miejsce_zamieszkania" onChange={(e)=> {
+setMiejsce_zamieszkania(e.target.value)
+}}/>
+      <label>Numer telefonu:</label>
+      <input type="text" name="numer_telefonu" onChange={(e)=> {
+setNumer_telefonu(e.target.value)
+}}/>
 
       <button onClick={submitKlient}>Wyślij</button>
+
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -17,19 +17,30 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", (req, res) => {
-    console.log("Działa");
-    res.json({});
+    res.json("Serwer działa");
+});
+
+app.get("/api/get", (req, res) => {
+
+    const sqlSelect = 
+    "SELECT * FROM klienci";
+    db.query(sqlSelect, (err, result) => {
+        res.send(result);
+    });
 });
 
 app.post("/api/insert", (req, res) => {
 
     const imie = req.body.imie
     const nazwisko = req.body.nazwisko
+    const pesel = req.body.pesel
+    const miejsce_zamieszkania = req.body.miejsce_zamieszkania
+    const numer_telefonu = req.body.numer_telefonu
 
     const sqlInsert = 
-    "INSERT INTO klienci (imie, nazwisko) VALUES (?,?)";
-    db.query(sqlInsert, [imie, nazwisko], (err, result) => {
-        console.log(err);
+    "INSERT INTO klienci (imie, nazwisko, pesel, miejsce_zamieszkania, numer_telefonu) VALUES (?,?,?,?,?)";
+    db.query(sqlInsert, [imie, nazwisko, pesel, miejsce_zamieszkania, numer_telefonu], (err, result) => {
+        console.log(result);
     });
     });
 
