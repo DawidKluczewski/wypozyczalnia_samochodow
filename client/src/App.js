c4e69a9848daa9fef94560f72ce4e2f689002102
Tajfunek100 <tajfun.sygu@gmail.com>
2022-05-26 22:07:19 +0200

    commit 4

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,17 @@ import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import Klient from './Klient';
 import Samochody from './Samochody';
 import Wypozyczenia from './Wypozyczenia';
+import './App2.css';
 function App() {
  return (
     <div className="App">
      <Router>
-          <Link to='/Klient'>Klient</Link>
-          <Link to='/Samochody'>Samochody</Link>
-          <Link to='/Wypozyczenia'>Wypożyczenia</Link>
+          <Link to='/Klient' className='klient'>Klient</Link>
+          <Link to='/Samochody' className='samochody'>Samochody</Link>
+          <Link to='/Wypozyczenia' className='wypozyczenia'>Wypożyczenia</Link>
           <Routes>
             <Route path='/'/>
-            <Route path='/Klient' element={<Klient />}/>
+            <Route path='/Klient'  element={<Klient />}/>
             <Route path='/Samochody' element={<Samochody />}/>
             <Route path='/Wypozyczenia' element={<Wypozyczenia />}/>
           </Routes>
