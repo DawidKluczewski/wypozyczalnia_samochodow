e505cfc66dd1563ef0989da118fd8956d52efae5
siemowit <IDawid923@gmail.com>
2022-06-01 20:59:43 +0200

    commicik

---
+++ client/src/TabelaWypozyczenia.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import logo from "../src/logo6969.png";
+
+function TabelaWypozyczenia() {
+  return (
+    <div className="klient">
+        <div className='srodek'>
+    
+          <div className='logo'>
+            <img src={logo} alt="this is car image" />
+          </div>
+    
+          <div className='tekst'> 
+            <h1>WYPOŻYCZALNIA SAMOCHODÓW</h1>
+          </div>
+          <div className='tekst2'>
+          <h2>Aktualne wypożyczenia</h2>
+          </div>
+    
+        </div>
+    </div>
+  )
+}
+
+export default TabelaWypozyczenia
---
+++ client/src/Wypozyczenia2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Wypozyczenia2() {
+  return (
+    <div>Wypozyczenia2</div>
+  )
+}
+
+export default Wypozyczenia2
